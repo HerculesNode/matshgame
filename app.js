aea297e43d5ddc110edf6fc48dda5091e1072429
Hercules <101635385+HerculesNode@users.noreply.github.com>
2025-02-21 22:28:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 5555); 
+app.set('port', process.env.PORT || 6356); 
 app.set('views', __dirname + '/app/server/views');
 app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + '/app/public'));
